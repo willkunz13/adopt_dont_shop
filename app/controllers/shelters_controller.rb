3fb745da4fce15fdc81de0f90512a01f8b5bbbd2
jerry <jerry@jerrys-MacBook-Pro.local>
2020-01-30 13:08:51 -0700

    Add shelters controller up through story 6

---
+++ app/controllers/shelters_controller.rb
@@ -0,0 +1,51 @@
+# app/controllers/shelters_controller.rb
+
+class SheltersController < ApplicationController
+
+	def index
+    		@shelters = Shelter.all
+  	end
+ 
+	def new
+	end
+
+        def create
+                shelter = Shelter.new({
+                        name: params[:shelter][:name],
+                        address: params[:shelter][:address],
+                        city: params[:shelter][:city],
+                        state: params[:shelter][:state],
+                        zip: params[:shelter][:zip]
+                        })
+
+                shelter.save
+                redirect_to '/shelters'
+        end
+
+        def show
+                @shelter = Shelter.find(params[:id])
+        end
+        
+        def edit
+                @shelter = Shelter.find(params[:id])
+        end
+
+        def update
+                shelter = Shelter.find(params[:id])
+                shelter.update({
+                 name: params[:shelter][:name],
+                 address: params[:shelter][:address],
+                 city: params[:shelter][:city],
+                 state: params[:shelter][:state],
+                 zip: params[:shelter][:zip]
+                    })
+                shelter.save
+                  redirect_to "/shelters/#{shelter.id}"       
+        end
+
+        def destroy
+                Shelter.destroy(params[:id])
+                redirect_to '/shelters'
+        end
+
+end
