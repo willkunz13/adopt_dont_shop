63d0889d50816f884b50874658d2908711d7861a
jerry <jerry@jerrys-MacBook-Pro.local>
2020-01-31 11:07:09 -0700

    Add routes up to date with story 9

--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,7 @@ Rails.application.routes.draw do
         get '/shelters/:id/edit', to: 'shelters#edit'
         patch '/shelters/:id', to: 'shelters#update'
         delete '/shelters/:id', to: 'shelters#destroy'
+	get '/pets', to: 'pets#index'
+	get '/pets/:id', to: 'pets#show'
+	get '/shelters/:shelter_id/pets', to: 'shelter_pets#index'
 end
