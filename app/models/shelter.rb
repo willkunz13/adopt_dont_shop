1ba896c769603f4a195754a99f83ed38e210b74f
jerry <jerry@jerrys-MacBook-Pro.local>
2020-01-31 11:03:00 -0700

    Add shelter model to have many pets

--- app/models/shelter.rb
+++ app/models/shelter.rb
@@ -1,6 +1,12 @@
 # app/models/shelter.rb
 
 class Shelter < ApplicationRecord
-
+	has_many :pets
+	
+	validates_presence_of :name
+	validates_presence_of :address
+	validates_presence_of :city
+	validates_presence_of :state
+	validates_presence_of :zip
 end
 
