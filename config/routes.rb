a0e42d9dda7f1f0a906e04b12272b07e44d9bae9
jerry <jerry@jerrys-MacBook-Pro.local>
2020-02-02 12:10:41 -0700

    Update routes to have patch and delete for pets

--- config/routes.rb
+++ config/routes.rb
@@ -13,4 +13,9 @@ Rails.application.routes.draw do
 	get '/pets', to: 'pets#index'
 	get '/pets/:id', to: 'pets#show'
 	get '/shelters/:shelter_id/pets', to: 'shelter_pets#index'
+	get '/shelters/:shelter_id/pets/new', to: 'shelter_pets#new'
+	post '/shelters/:shelter_id/pets', to: 'shelter_pets#create'
+	get '/pets/:id/edit', to: 'pets#edit'
+	patch '/pets/:id', to: 'pets#update'
+	delete '/pets/:id', to: 'pets#destroy'
 end
