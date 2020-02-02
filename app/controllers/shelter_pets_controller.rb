c54c4220c3209a4898874a87adefc207379be5a0
jerry <jerry@jerrys-MacBook-Pro.local>
2020-02-02 12:11:54 -0700

    Add functionality to edit shelter pets

--- app/controllers/shelter_pets_controller.rb
+++ app/controllers/shelter_pets_controller.rb
@@ -1,11 +1,21 @@
 class ShelterPetsController < ApplicationController
 
 	def index
-		@pets = Shelter.find(params[:shelter_id]).pets
+		@shelter = Shelter.find(params[:shelter_id])
 	end
 
+	def new
+	end
+
+	def create
+		pet = Pet.create!(pet_params)
+		redirect_to "/shelters/#{pet_params[:shelter_id]}/pets"
+		
+	end
+
+	private
+	def pet_params
+		params.permit( :name, :image, :description, :approximate_age,\
+			:sex, :shelter_id)
+	end
 end
-
-
-
-
