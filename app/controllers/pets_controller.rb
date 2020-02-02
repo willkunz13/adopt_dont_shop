8e533c4277fe48d27346c67e9547f1c899a77a8a
jerry <jerry@jerrys-MacBook-Pro.local>
2020-02-02 12:13:37 -0700

    Add delete functionality to pets

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -8,4 +8,26 @@ class PetsController < ApplicationController
 		@pet = Pet.find(params[:id])
 	end
 
+	def edit
+		@pet = Pet.find(params[:id])
+	end
+
+	def update
+		pet = Pet.find(params[:id])
+		pet.update(pet_params)
+		redirect_to "/pets/#{pet.id}"
+	end
+
+	def destroy
+		Pet.destroy(params[:id])
+		redirect_to '/pets'
+	end
+
+	private
+
+	def pet_params
+		params.permit( :name, :image, :description, \
+			:approximate_age, :sex)
+	end
+
 end
