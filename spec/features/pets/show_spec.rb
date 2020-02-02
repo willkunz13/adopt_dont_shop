875167a0dcd3ef7840ace0c16e99a1b0149c0f8e
jerry <jerry@jerrys-MacBook-Pro.local>
2020-02-02 12:06:46 -0700

    Update tests to match changes to pet migration

--- spec/features/pets/show_spec.rb
+++ spec/features/pets/show_spec.rb
@@ -5,9 +5,9 @@ RSpec.describe "pets show page", type: :feature do
 		shelter_1 = Shelter.create(name: "helter_shelter", \
 address: "nowhere", city: "Detroit", state: "MI", zip: "313")
 		pet_1 = Pet.create(name: "dog", image: "/assets/dog_test.jpg", approximate_age: 3, \
-sex: "F", description: "big", adopt_status: "not",  shelter: shelter_1)
+sex: "F", description: "big", shelter: shelter_1)
 		pet_2 = Pet.create(name: "tiger", image: "/assets/tiger_test.jpg", approximate_age: 7, \
-sex: "M", description: "small", adopt_status: "not", shelter: shelter_1)
+sex: "M", description: "small", shelter: shelter_1)
 
 		visit "/pets/#{pet_1.id}"
 
@@ -16,4 +16,21 @@ sex: "M", description: "small", adopt_status: "not", shelter: shelter_1)
 		expect(page).to have_content(pet_1.adopt_status)
 		expect(page).to_not have_content(pet_2.name)
 	end
+
+	it "can destroy a pet" do
+		shelter_1 = Shelter.create(name: "helter_shelter", \
+address: "nowhere", city: "Detroit", state: "MI", zip: "313")
+		pet_1 = Pet.create(name: "dog", image: "/assets/dog_test.jpg", approximate_age: 3, \
+sex: "F", description: "big", shelter: shelter_1)
+		pet_2 = Pet.create(name: "tiger", image: "/assets/tiger_test.jpg", approximate_age: 7, \
+sex: "M", description: "small", shelter: shelter_1)
+
+                visit "/pets/#{pet_1.id}"
+		
+		expect(page).to have_content("dog")
+		
+		click_button "Delete Pet"
+
+		expect(page).to_not have_content("dog")
+	end			
 end
